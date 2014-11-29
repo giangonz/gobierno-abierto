ed0dde5d4a2d9440444600347c228210aa5060eb
Sasha Méndez <sasha.mendez@gmail.com>
2014-11-29 03:29:37 -0400

    Added Heroku settings.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dashboard_gobernacion.wsgi --log-file -
--- dashboard_gobernacion/settings.py
+++ dashboard_gobernacion/settings.py
@@ -97,3 +97,11 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+# Heroku settings
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
