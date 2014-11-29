65e9866d867e78ba3e9cfbb88c2ba6644f9ae359
Sasha Méndez <sasha.mendez@gmail.com>
2014-11-29 04:49:43 -0400

    Added Static folder. Added whitenoise.

--- dashboard_gobernacion/settings.py
+++ dashboard_gobernacion/settings.py
@@ -83,8 +83,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
--- dashboard_gobernacion/wsgi.py
+++ dashboard_gobernacion/wsgi.py
@@ -11,6 +11,8 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dashboard_gobernacion.settings")
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
